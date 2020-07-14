7f799eced9c709e49f16f82457a1bd712846cc7e
James Ward <james@jamesward.com>
2020-07-14 12:13:14 -0600

    blah

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,10 @@ import com.google.protobuf.gradle.plugins
 import com.google.protobuf.gradle.protobuf
 import com.google.protobuf.gradle.protoc
 
-val grpcVersion = "1.30.0"
-val protobufVersion = "3.11.4"
-val grpcKotlinVersion = "0.1.3"
+val grpcVersion = "1.30.2"
+val protobufVersion = "3.12.2"
+val grpcKotlinVersion = "0.1.4"
 val coroutinesVersion = "1.3.7"
-
-//val protobufVersion = "3.12.2"
 
 plugins {
     application
@@ -26,18 +24,19 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
     implementation("com.google.protobuf:protobuf-java:$protobufVersion")
     implementation("com.google.protobuf:protobuf-java-util:$protobufVersion")
     implementation("io.cloudstate:cloudstate-kotlin-support:0.5.1")
-    implementation("io.grpc:grpc-stub:$grpcVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
     implementation("io.grpc:grpc-protobuf:$grpcVersion")
     implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
     implementation("com.google.api.grpc:proto-google-common-protos:1.18.0")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
     implementation("org.apache.lucene:lucene-spatial:8.4.1")
     implementation("com.devskiller:jfairy:0.6.4")
+
+    runtimeOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
 protobuf {
@@ -71,19 +70,6 @@ sourceSets {
     main.java.srcDirs("build/generated/source/proto/main/java")
     main.java.srcDirs("build/generated/source/proto/main/grpc")
     main.java.srcDirs("build/generated/source/proto/main/grpckt")
-
-    /*
-    main {
-
-        java {
-            srcDirs("build/generated/source/proto/main/grpc")
-            srcDirs("build/generated/source/proto/main/java")
-        }
-        kotlin {
-            srcDirs("")
-        }
-    }
-     */
 }
 
 application {
